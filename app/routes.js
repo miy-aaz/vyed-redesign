543ebd4f3ee5dab06d602a28b1ac391ad4411896
AZIZ <Miya.AZIZ@EDUCATION.GOV.UK>
2026-02-06 14:35:23 +0000

    added sign in options

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,6 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
 router.post('/organisation-type', (req, res) => {
     if(req.body.new.organisationType == 'The Department for Education') {
         res.redirect('/internal-sign-in')
